8ff69e38405c9d32ce105243888caac7299c42bc
Steve <mittosteve@gmail.com>
2019-09-23 08:24:30 +0300

    This commit allows the writer to delete comments they dont like

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,5 +1,5 @@
 from . import blog
-from flask import render_template,jsonify,request,url_for,g
+from flask import render_template,jsonify,request,url_for,redirect
 from flask_login import login_required,current_user
 from app.models import Blog,Comment
 from app import photos
@@ -54,3 +54,18 @@ def save_comment(blog_id):
     new_comment = Comment(comment = comment, user_id = user.id , blog_id = blog_id)
     new_comment.save()
     return jsonify({'success':'Your comment was added'})
+
+@blog.route('/blog/<blog_id>/delete/<comment_id>/comment')
+def delete_comment(comment_id,blog_id):
+    blog = Blog.query.filter_by(id = blog_id).first()
+    if blog.user.id  == user.id:
+        comment = Comment.query.filter_by(id = comment_id).first()
+        if comment == None:
+            return jsonify({'error':'sorry that comment was already deleted'})
+        else:
+            comment.delete()
+            # return jsonify({'success':'Comment deleted'})
+            return redirect(url_for('blog.blog_details',blog_id = blog_id))
+
+    else:
+        abort(404)
--- app/static/js/blog/blog_details.js
+++ app/static/js/blog/blog_details.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-  console.log('/blog/'+ $("#blog_id").val()+'/save_comment');
   $("#add_comment").submit(function(event){
     $.post('/blog/'+ $("#blog_id").val()+'/save_comment',
     {
@@ -19,5 +18,21 @@ $(document).ready(function(){
 
     console.log($("#comment").val());
     event.preventDefault()
+  });
+  $(".delete").click(function(){
+    console.log('clicked');
+    console.log($(".delete").children(".comment_id") .val());
+    // console.log(".delete"+$("#comment_id").val()+"");
+    // $.get('/blog/'+ $("#blog_id").val()+'/delete/'+ $("#comment_id").val()+'/comment',
+    //   function(data){
+    //     if(data.error){
+    //       console.log(data.error);
+    //     }
+    //     if(data.success){
+    //       $("#comment",$("#comment_id").val()).fadeOut(1000)
+    //       console.log(data.success);
+    //       $("#comment"+$('#comment_id').val()+"").slideUp(1000)
+    //     }
+    //   });
   });
 });
